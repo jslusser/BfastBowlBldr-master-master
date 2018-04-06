fb0a477ce5961dfb43d549dee5517cd3928b342e
jslusser <jslusser@gmail.com>
2018-04-06 17:30:16 -0400

    Installed Crashlytics

--- BfastBowlBldr/ViewControllers/MainMenuTableViewController.swift
+++ BfastBowlBldr/ViewControllers/MainMenuTableViewController.swift
@@ -7,12 +7,12 @@
 //
 
 import UIKit
+import Crashlytics
 
 class MainMenuTableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
