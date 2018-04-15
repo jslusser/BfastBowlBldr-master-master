bbc39265a8840399d35b2c87339a52307918415f
jslusser <jslusser@gmail.com>
2018-04-15 18:17:30 -0400

    Added and configured Answers in my Fabric account [part of Fabric/Crashlytics] - see MainMenuTableViewController.swift

--- BfastBowlBldr/ViewControllers/MainMenuTableViewController.swift
+++ BfastBowlBldr/ViewControllers/MainMenuTableViewController.swift
@@ -13,10 +13,18 @@ import UserNotifications
 class MainMenuTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Still need to build out the notifications, likely in a future version when I have a indiviual user recipe database and am using CloudKit
+//        Comment out the following - customize when ready to use Answers, but the code is already installed and configured in my Fabric account [part of Fabric/Crashlytics]
+//        let button = UIButton(type: UIButtonType.roundedRect)
+//        button.setTitle("Trigger Key Metric", for: [])
+//        button.addTarget(self, action: #selector(self.anImportantUserAction), for: UIControlEvents.touchUpInside)
+//        button.sizeToFit()
+//        button.center = self.view.center
+//        view.addSubview(button)
+
+//        Still need to build out the notifications, likely in a future version when I have a indiviual user recipe database and am using CloudKit
          registerLocal()
-        // Commented out the following until I resume Crashlytics testing prn
-        // Crashlytics.sharedInstance().crash()
+//        Commented out the following until I resume Crashlytics testing prn
+//        Crashlytics.sharedInstance().crash()
     }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -28,6 +36,15 @@ class MainMenuTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 5
     }
+//        Comment out the following - customize when ready to implement Answers (part of Fabric/Crashlytics)
+//      @objc func anImportantUserAction() {
+//        TODO: Move this method and customize the name and parameters to track your key metrics
+//        Use your own string attributes to track common values over time
+//        Use your own number attributes to track median value over time
+//        Answers.logCustomEvent(withName: "Video Played", customAttributes: ["Category":"Comedy", "Length":350])
+//    }
+
+    
     func registerLocal() {
         let center = UNUserNotificationCenter.current()
         center.requestAuthorization(options: [.alert, .badge, .sound]) { (granted, _) in
